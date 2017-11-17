67f82e8b18c79a35baef992e734da58366419bf8
Liz Davies <sarah.e.e.davies@gmail.com>
2017-11-16 20:33:47 -0500

    Attribution addition

--- myTunesCollection.h
+++ myTunesCollection.h
@@ -1,4 +1,20 @@
-//Project by: Sarah Davies - 100828244 Mike Sayegh - 101029473
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                   */
+/*  Program:  MyTunes Music Player                   */
+/*  Author:   Louis Nel								 */
+/*			  Sarah Davies  - 100828244				 */
+/*			  Mike Sayegh	- 101029473				 */
+/*  Date:     21-SEP-2017                            */
+/*                                                   */
+/*  (c) 2017 Louis Nel                               */
+/*  All rights reserved.  Distribution and           */
+/*  reposting, in part or in whole, requires         */
+/*  written consent of the author.                   */
+/*                                                   */
+/*  COMP 2404 students may reuse this content for    */
+/*  their course assignments without seeking consent */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 
 #ifndef _MYTUNESCOL_H
 #define _MYTUNESCOL_H
--- mytunesmodel.cpp
+++ mytunesmodel.cpp
@@ -1,3 +1,20 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                   */
+/*  Program:  MyTunes Music Player                   */
+/*  Author:   Louis Nel								 */
+/*			  Sarah Davies  - 100828244				 */
+/*			  Mike Sayegh	- 101029473				 */
+/*  Date:     21-SEP-2017                            */
+/*                                                   */
+/*  (c) 2017 Louis Nel                               */
+/*  All rights reserved.  Distribution and           */
+/*  reposting, in part or in whole, requires         */
+/*  written consent of the author.                   */
+/*                                                   */
+/*  COMP 2404 students may reuse this content for    */
+/*  their course assignments without seeking consent */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include "mytunesmodel.h"
 using namespace std;
 MyTunesModel::MyTunesModel(){}
