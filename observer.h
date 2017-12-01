65b9ba166a7952cba5fcf156543a414886b6b196
mikesayegh <michaelsayegh37@gmail.com>
2017-12-01 12:34:23 -0500

    adding observer and subject classes

---
+++ observer.h
@@ -0,0 +1,18 @@
+#include <string.h>
+
+class Subject;
+class Observer {
+  public:
+  virtual void update(Subject * subject)= 0;
+
+  virtual void printOn(ostream & out) const = 0;
+
+  int operator==(const Observer & obs) const {
+    return this == & obs;
+  } 
+};
+
+ostream & operator<<(ostream & out, const Observer & obs) {
+ obs.printOn(out);
+ return out;
+}
---
+++ subject.h
@@ -0,0 +1,22 @@
+class Subject {
+//    =======
+ ArrayList<Observer> observers;
+public:
+  void attach(Observer & obs) {
+   observers.add(obs);
+  //cout << "\nAttach: " << " observers size: " << observers.size() << "\n";
+
+  }
+  void dettach(Observer & obs) {
+   observers.remove(obs);
+   //cout << "\nDettach: " << " observers size: " << observers.size() << "\n";
+  }
+
+protected:
+  void notify() {
+    ArrayList<Observer> observers_copy = observers;
+   for(ArrayList<Observer>::iterator itr = observers_copy.begin(); itr != observers_copy.end(); itr++) {
+      (*itr).update((Subject*) this);
+   }
+  }
+};
